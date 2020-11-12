f334ba95ec803e5f26068a352a1f1f861aeac087
Muratcan Şentürk <muratcansenturk2000@hotmail.com>
2020-11-12 13:41:11 +0300

    set server to local network

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  server: {
+    host: '0' // default: localhost
+  },
   head: {
     title: 'thing2.0',
     meta: [
