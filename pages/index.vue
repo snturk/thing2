c383597440313a32472d46662765dce0dc800862
Muratcan Şentürk <muratcansenturk2000@hotmail.com>
2020-11-13 13:04:36 +0300

    Revert "responsive changes"
    This reverts commit 03313613d39d9ba933cdf8ec7ee098ffad26281b.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container">
     <div id="homeTitle">Place where you&nbsp;<span>relax</span></div>
-    <div id="homeSubtitle">click below to start</div>
-    <div id="homeCircle"></div>
   </div>
 </template>
 
@@ -13,45 +11,23 @@ export default {}
 <style>
 .container{
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 }
 
 #homeTitle{
+  width: 50%;
   text-align: center;
   display: flex;
-  text-align: center;
+  margin: auto;
   flex-direction: row;
-  font-size: 36px;
+  font-size: 64px;
   font-weight: bold;
   color: white;
-  text-shadow: 2px 2px 5px rgba(189, 189, 189, 0.4);
 }
+
 #homeTitle span{
   font-weight: 100;
-  text-shadow: none;
-}
-
-#homeSubtitle{
-  color: white;
-  font-size: 17px;
-  margin-top: 2%;
-}
-
-#homeCircle{
-  padding: 4%;
-  margin-top: 5%;
-  background: #8c8cd9;
-  border-radius: 50%;
-  box-shadow: 1px 1px 10px #3e3ecc5d;
-  animation-name: homeCircle;
-  animation-duration: 4.4s;
-  animation-iteration-count: infinite;
-}
-@keyframes homeCircle {
-  0%{box-shadow: 1px 1px 10px #3e3eccfa; background: #8c8cd9;}
-  50%{box-shadow: 1px 1px 100px #3e3eccfa; background: rgb(154, 154, 199);}
-  100%{box-shadow: 1px 1px 10px #3e3eccfa; background: #8c8cd9;}
 }
 </style>
